5b897292d460a4097ae240355da89af120cd0059
Vedant Kumar <vk@vedantk.com>
2011-12-27 00:31:50 -0500

    Adds support for dropping root privileges.

--- events.cpp
+++ events.cpp
@@ -6,7 +6,9 @@
 #include "schedule.h"
 #include <cerrno>
 
-
+// chroot, setuid
+#include <unistd.h>
+#include <sys/types.h>
 
 // Define the connection mother (first half) and connection middlemen (second half)
 
@@ -17,40 +19,40 @@
 connection_mother::connection_mother(worker * worker_obj, config * config_obj, mysql * db_obj) : work(worker_obj), conf(config_obj), db(db_obj) {
 	open_connections = 0;
 	opened_connections = 0;
-	
+
 	memset(&address, 0, sizeof(address));
 	addr_len = sizeof(address);
-	
+
 	listen_socket = socket(AF_INET, SOCK_STREAM, 0);
-	
+
 	// Stop old sockets from hogging the port
 	int yes = 1;
 	if(setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
 		std::cout << "Could not reuse socket" << std::endl;
 	}
-	
+
 	// Create libev event loop
 	ev::io event_loop_watcher;
-	
+
 	event_loop_watcher.set<connection_mother, &connection_mother::handle_connect>(this);
 	event_loop_watcher.start(listen_socket, ev::READ);
-	
+
 	// Get ready to bind
 	address.sin_family = AF_INET;
 	//address.sin_addr.s_addr = inet_addr(conf->host.c_str()); // htonl(INADDR_ANY)
 	address.sin_addr.s_addr = htonl(INADDR_ANY);
 	address.sin_port = htons(conf->port);
-	
+
 	// Bind
 	if(bind(listen_socket, (sockaddr *) &address, sizeof(address)) == -1) {
 		std::cout << "Bind failed " << errno << std::endl;
 	}
-	
+
 	// Listen
 	if(listen(listen_socket, conf->max_connections) == -1) {
 		std::cout << "Listen failed" << std::endl;
 	}
-	
+
 	// Set non-blocking
 	int flags = fcntl(listen_socket, F_GETFL);
 	if(flags == -1) {
@@ -59,14 +61,24 @@ connection_mother::connection_mother(worker * worker_obj, config * config_obj, m
 	if(fcntl(listen_socket, F_SETFL, flags | O_NONBLOCK) == -1) {
 		std::cout << "Could not set non-blocking" << std::endl;
 	}
-	
+
 	// Create libev timer
 	schedule timer(this, worker_obj, conf, db);
-	
+
 	schedule_event.set<schedule, &schedule::handle>(&timer);
 	schedule_event.set(conf->schedule_interval, conf->schedule_interval); // After interval, every interval
 	schedule_event.start();
-	
+
+	if (!getuid()) {
+		std::cout << "Dropping root privileges..\n";
+		if (chroot(".") < 0) {
+			std::cerr << "Warning: couldn't chroot, " << strerror(errno) << std::endl;
+		}
+		if (!setuid(1000) < 0) {
+			std::cerr << "Warning: couldn't setuid, " << strerror(errno) << std::endl;
+		}
+	}
+
 	std::cout << "Sockets up, starting event loop!" << std::endl;
 	ev_loop(ev_default_loop(0), 0);
 }
@@ -93,9 +105,9 @@ connection_mother::~connection_mother()
 
 //---------- Connection middlemen - these little guys live until their connection is closed
 
-connection_middleman::connection_middleman(int &listen_socket, sockaddr_in &address, socklen_t &addr_len, worker * new_work, connection_mother * mother_arg, config * config_obj) : 
+connection_middleman::connection_middleman(int &listen_socket, sockaddr_in &address, socklen_t &addr_len, worker * new_work, connection_mother * mother_arg, config * config_obj) :
 	conf(config_obj), mother (mother_arg), work(new_work) {
-	
+
 	connect_sock = accept(listen_socket, (sockaddr *) &address, &addr_len);
 	if(connect_sock == -1) {
 		std::cout << "Accept failed, errno " << errno << ": " << strerror(errno) << std::endl;
@@ -103,7 +115,7 @@ connection_middleman::connection_middleman(int &listen_socket, sockaddr_in &addr
 		delete this;
 		return;
 	}
-	
+
 	// Set non-blocking
 	int flags = fcntl(connect_sock, F_GETFL);
 	if(flags == -1) {
@@ -112,21 +124,21 @@ connection_middleman::connection_middleman(int &listen_socket, sockaddr_in &addr
 	if(fcntl(connect_sock, F_SETFL, flags | O_NONBLOCK) == -1) {
 		std::cout << "Could not set non-blocking" << std::endl;
 	}
-	
+
 	// Get their info
 	if(getpeername(connect_sock, (sockaddr *) &client_addr, &addr_len) == -1) {
 		//std::cout << "Could not get client info" << std::endl;
 	}
-	
-	
+
+
 	read_event.set<connection_middleman, &connection_middleman::handle_read>(this);
 	read_event.start(connect_sock, ev::READ);
-	
+
 	// Let the socket timeout in timeout_interval seconds
 	timeout_event.set<connection_middleman, &connection_middleman::handle_timeout>(this);
 	timeout_event.set(conf->timeout_interval, 0);
 	timeout_event.start();
-	
+
 	mother->increment_open_connections();
 }
 
@@ -138,27 +150,27 @@ connection_middleman::~connection_middleman() {
 // Handler to read data from the socket, called by event loop when socket is readable
 void connection_middleman::handle_read(ev::io &watcher, int events_flags) {
 	read_event.stop();
-	
+
 	char buffer[conf->max_read_buffer + 1];
 	memset(buffer, 0, conf->max_read_buffer + 1);
 	int status = recv(connect_sock, &buffer, conf->max_read_buffer, 0);
-	
+
 	if(status == -1) {
 		delete this;
 		return;
 	}
-	
+
 	std::string stringbuf = buffer;
-	
+
 	char ip[INET_ADDRSTRLEN];
 	inet_ntop(AF_INET, &(client_addr.sin_addr), ip, INET_ADDRSTRLEN);
 	std::string ip_str = ip;
-	
+
 	//--- CALL WORKER
 	response = work->work(stringbuf, ip_str);
-	
-	// Find out when the socket is writeable. 
-	// The loop in connection_mother will call handle_write when it is. 
+
+	// Find out when the socket is writeable.
+	// The loop in connection_mother will call handle_write when it is.
 	write_event.set<connection_middleman, &connection_middleman::handle_write>(this);
 	write_event.start(connect_sock, ev::WRITE);
 }
