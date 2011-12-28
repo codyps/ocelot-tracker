f6d01f36bb4d42467f46a9f7a76446117bf319b2
Vedant Kumar <vk@vedantk.com>
2011-12-28 00:04:02 -0500

    Basic ipv6 support implemented.

--- events.cpp
+++ events.cpp
@@ -10,55 +10,63 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+// inttostr
+#include "misc_functions.h"
+
 // Define the connection mother (first half) and connection middlemen (second half)
 
 //TODO Better errors
 
 //---------- Connection mother - spawns middlemen and lets them deal with the connection
 
-connection_mother::connection_mother(worker * worker_obj, config * config_obj, mysql * db_obj) : work(worker_obj), conf(config_obj), db(db_obj) {
+connection_mother::connection_mother(worker * worker_obj, config * config_obj, mysql * db_obj) : sock(-1), work(worker_obj), conf(config_obj), db(db_obj) {
 	open_connections = 0;
 	opened_connections = 0;
 
-	memset(&address, 0, sizeof(address));
-	addr_len = sizeof(address);
+	ai = getnetinfo(conf->host.c_str(), conf->port, SOCK_STREAM);
+	if (!ai) {
+		std::cerr << "getnetinfo failed.. " << errno << std::endl;
+	}
 
-	listen_socket = socket(AF_INET, SOCK_STREAM, 0);
+	struct addrinfo *rp;
+	for (rp = ai; rp != NULL; rp = rp->ai_next) {
+		sock = socket(rp->ai_family, rp->ai_socktype,
+			      rp->ai_protocol);
+		if (sock < 0) continue;
 
-	// Stop old sockets from hogging the port
-	int yes = 1;
-	if(setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-		std::cout << "Could not reuse socket" << std::endl;
+		int ret = 1;
+		int err = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR,
+				     &ret, 4);
+		if (err != 0) {
+			std::cout << "Could not reuse socket" << std::endl;
+		}
+
+		if (bind(sock, rp->ai_addr, rp->ai_addrlen) == 0) {
+			break;
+		}
+	}
+
+	if (rp == NULL) {
+		perror("Bind failed");
 	}
 
 	// Create libev event loop
 	ev::io event_loop_watcher;
 
 	event_loop_watcher.set<connection_mother, &connection_mother::handle_connect>(this);
-	event_loop_watcher.start(listen_socket, ev::READ);
-
-	// Get ready to bind
-	address.sin_family = AF_INET;
-	//address.sin_addr.s_addr = inet_addr(conf->host.c_str()); // htonl(INADDR_ANY)
-	address.sin_addr.s_addr = htonl(INADDR_ANY);
-	address.sin_port = htons(conf->port);
-
-	// Bind
-	if(bind(listen_socket, (sockaddr *) &address, sizeof(address)) == -1) {
-		std::cout << "Bind failed " << errno << std::endl;
-	}
+	event_loop_watcher.start(sock, ev::READ);
 
 	// Listen
-	if(listen(listen_socket, conf->max_connections) == -1) {
-		std::cout << "Listen failed" << std::endl;
+	if(listen(sock, conf->max_connections) == -1) {
+		perror("Listen failed");
 	}
 
 	// Set non-blocking
-	int flags = fcntl(listen_socket, F_GETFL);
+	int flags = fcntl(sock, F_GETFL);
 	if(flags == -1) {
 		std::cout << "Could not get socket flags" << std::endl;
 	}
-	if(fcntl(listen_socket, F_SETFL, flags | O_NONBLOCK) == -1) {
+	if(fcntl(sock, F_SETFL, flags | O_NONBLOCK) == -1) {
 		std::cout << "Could not set non-blocking" << std::endl;
 	}
 
@@ -88,13 +96,13 @@ void connection_mother::handle_connect(ev::io &watcher, int events_flags) {
 	// Spawn a new middleman
 	if(open_connections < conf->max_middlemen) {
 		opened_connections++;
-		new connection_middleman(listen_socket, address, addr_len, work, this, conf);
+		new connection_middleman(sock, ai->ai_addr, ai->ai_addrlen, work, this, conf);
 	}
 }
 
 connection_mother::~connection_mother()
 {
-	close(listen_socket);
+	close(sock);
 }
 
 
@@ -105,10 +113,10 @@ connection_mother::~connection_mother()
 
 //---------- Connection middlemen - these little guys live until their connection is closed
 
-connection_middleman::connection_middleman(int &listen_socket, sockaddr_in &address, socklen_t &addr_len, worker * new_work, connection_mother * mother_arg, config * config_obj) :
+connection_middleman::connection_middleman(int &listen_socket, struct sockaddr* address, socklen_t &addr_len, worker * new_work, connection_mother * mother_arg, config * config_obj) :
 	conf(config_obj), mother (mother_arg), work(new_work) {
 
-	connect_sock = accept(listen_socket, (sockaddr *) &address, &addr_len);
+	connect_sock = accept(listen_socket, address, &addr_len);
 	if(connect_sock == -1) {
 		std::cout << "Accept failed, errno " << errno << ": " << strerror(errno) << std::endl;
 		mother->increment_open_connections(); // destructor decrements open connections
@@ -191,4 +199,21 @@ void connection_middleman::handle_timeout(ev::timer &watcher, int events_flags) 
 	read_event.stop();
 	write_event.stop();
 	delete this;
+}
+
+struct addrinfo* getnetinfo(const char* host, int port, int socktype)
+{
+	struct addrinfo hints;
+	struct addrinfo* result;
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = socktype;
+	hints.ai_flags = AI_PASSIVE;
+	std::string s_port = inttostr(port);
+
+	int err;
+	if ((err = getaddrinfo(host, s_port.c_str(), &hints, &result)) != 0) {
+		std::cerr << "[getaddrinfo] " << gai_strerror(err) << std::endl;
+	}
+	return result;
 }
--- events.h
+++ events.h
@@ -9,25 +9,15 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
-
-
-
-/*
-TODO find out what these do
 #include <unistd.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <sys/types.h>
-*/
-
-
-
-
 
 /*
 We have three classes - the mother, the middlemen, and the worker
 THE MOTHER
-	The mother is called when a client opens a connection to the server. 
+	The mother is called when a client opens a connection to the server.
 	It creates a middleman for every new connection, which will be called
 	when its socket is ready for reading.
 THE MIDDLEMEN
@@ -35,35 +25,32 @@ THE MIDDLEMEN
 	reads the data and sends it to the worker. When it gets the response, it
 	gets called to write its data back to the client.
 THE WORKER
-	The worker gets data from the middleman, and returns the response. It 
-	doesn't concern itself with silly things like sockets. 
-	
+	The worker gets data from the middleman, and returns the response. It
+	doesn't concern itself with silly things like sockets.
+
 	see worker.h for the worker.
 */
-
-
-
 
 // THE MOTHER - Spawns connection middlemen
 class connection_mother {
 	private:
-		int listen_socket;
-		sockaddr_in address;
-		socklen_t addr_len;
+		int sock;
+		struct addrinfo* ai;
+
 		worker * work;
 		config * conf;
 		mysql * db;
 		ev::timer schedule_event;
-		
+
 		unsigned long opened_connections;
 		unsigned int open_connections;
-		
-	public: 
+
+	public:
 		connection_mother(worker * worker_obj, config * config_obj, mysql * db_obj);
-		
+
 		void increment_open_connections() { open_connections++; }
 		void decrement_open_connections() { open_connections--; }
-		
+
 		int get_open_connections() { return open_connections; }
 		int get_opened_connections() { return opened_connections; }
 
@@ -81,21 +68,20 @@ class connection_middleman {
 		ev::io write_event;
 		ev::timer timeout_event;
 		std::string response;
-		
+
 		config * conf;
 		connection_mother * mother;
 		worker * work;
 		sockaddr_in client_addr;
-	
+
 	public:
-		connection_middleman(int &listen_socket, sockaddr_in &address, socklen_t &addr_len, worker* work, connection_mother * mother_arg, config * config_obj);
+		connection_middleman(int &listen_socket, struct sockaddr* address, socklen_t &addr_len, worker* work, connection_mother * mother_arg, config * config_obj);
 		~connection_middleman();
-	
+
 		void handle_read(ev::io &watcher, int events_flags);
 		void handle_write(ev::io &watcher, int events_flags);
 		void handle_timeout(ev::timer &watcher, int events_flags);
 };
 
-
-
-
+/* Get information about a host. */
+struct addrinfo* getnetinfo(const char* host, int port, int proto);
